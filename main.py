f1e34a8b5d68e7faaa8d0a4aab56e8dc2cb6dc51
Jozef Cipa <cipa.jozef@gmail.com>
2018-01-24 22:34:03 +0100

    removed unused imports

--- main.py
+++ main.py
@@ -2,13 +2,6 @@
 
 from TorBrowser import TorBrowser
 import time
-from pyquery import PyQuery as pq
-from lxml import etree
-import requests
-import re
-import urllib.parse
-import ast
-import sys
 
 tor = TorBrowser(password="password")
 tor.connect()
